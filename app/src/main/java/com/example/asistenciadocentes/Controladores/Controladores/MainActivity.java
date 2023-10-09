a7cc9adc395b097f9cb9556282958e67c049e571
ElianTrem <eliantreminio11@gmail.com>
2023-10-08 19:27:36 -0600

    Proyecto Funcionando con git--Elian Modifications ya repare la chingadera que el sando me dijo

--- app/src/main/java/com/example/asistenciadocentes/Controladores/Controladores/MainActivity.java
+++ app/src/main/java/com/example/asistenciadocentes/Controladores/Controladores/MainActivity.java
@@ -174,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
                                     }
 
 
+
                                     @Override
                                     public void onCancelled(@NonNull DatabaseError databaseError) {
                                         // Manejar el error de la consulta
